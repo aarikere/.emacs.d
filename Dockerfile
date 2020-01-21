5fe7f162ed0df23311359a4a37b2d8b2bdc79da7
Adi <1927365+aarikere@users.noreply.github.com>
2020-01-21 11:12:06 +0100

    Change to Consolas font. Update dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -11,8 +11,7 @@ RUN git config --global user.email "adithya.arikere@volvo.com"
 RUN git config --global alias.cl 'log --oneline -n'
 RUN git config --global alias.cd 'diff --compact-summary'
 
-COPY fonts/Inconsolata-Bold.ttf /usr/share/fonts/
-COPY fonts/Inconsolata-Regular.ttf /usr/share/fonts/
+COPY fonts /usr/share/fonts/
 
 RUN echo "deb http://archive.ubuntu.com/ubuntu/ eoan main restricted" >> /etc/apt/sources.list
 RUN echo "deb http://archive.ubuntu.com/ubuntu/ eoan-updates main restricted" >> /etc/apt/sources.list
--- init.el
+++ init.el
@@ -69,7 +69,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
  '(font-lock-builtin-face ((t (:foreground "#268bd2" :slant italic :weight bold))))
  '(font-lock-doc-face ((t (:foreground "#2aa198" :slant italic)))))
 
