4c9d64ef2caa727422d16d847d92dd1f44eee2e3
Adi <1927365+aarikere@users.noreply.github.com>
2020-11-10 20:42:14 +0100

    Add rtags and update packages

---
+++ elpa/flycheck-rtags-20191222.920/flycheck-rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /mnt/c/Users/a323747/.emacs.d/elpa/flycheck-rtags-20191222.920/flycheck-rtags.el  -*- no-byte-compile: t -*-
+(define-package "flycheck-rtags" "20191222.920" "RTags Flycheck integration" '((emacs "24") (flycheck "0.23") (rtags "2.10")) :commit "63a7d9b733def9fe76568f81321a65a450829575" :authors '(("Christian Schwarzgruber" . "c.schwarzgruber.cs@gmail.com")) :maintainer '("Christian Schwarzgruber" . "c.schwarzgruber.cs@gmail.com") :url "https://github.com/Andersbakken/rtags")
